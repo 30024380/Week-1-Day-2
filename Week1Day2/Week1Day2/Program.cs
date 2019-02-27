4f85437ded73c8ab903af1dbf056ce7cb5f5dc84
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-02-28 12:24:24 +1300

    OK

---
+++ Week1Day2/Week1Day2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+        
+            int age;
+            string name;
+
+            Console.WriteLine("Enter Age Here");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Name Here");
+            name = Console.ReadLine();
+
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+        
+        }
+    }
+}
