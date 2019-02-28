cb329bcc6283353b76d4a87ea8b02fe8eff5a91d
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-02-28 13:17:29 +1300

    qwjeqowirj

--- Week1Day2/Week1Day2/Program.cs
+++ Week1Day2/Week1Day2/Program.cs
@@ -13,18 +13,42 @@ namespace Week1Day2
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
 
-        
+            int pin;
             int age;
-            string name;
+            Decimal one;
+            Decimal two;
 
-            Console.WriteLine("Enter Age Here");
+            Console.WriteLine("Enter Your Pin");
+             pin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Age Here");
             age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Name Here");
-            name = Console.ReadLine();
+            Console.WriteLine("Enter Your First Name Here");
+            string firstname = Console.ReadLine();
 
-            Console.WriteLine(age);
-            Console.WriteLine(name);
+            Console.WriteLine("Enter Your Last Name Here");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Address Here");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Email Address Here");
+            string eaddress = Console.ReadLine();
+
+            Console.WriteLine("Enter First Decimal Here");
+            one = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Decimal Here");
+            two = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Details");
+            Console.WriteLine("Age: " +age);
+            Console.WriteLine("Name: " + firstname + " " + lastname);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Email Address: " + eaddress);
+            Console.WriteLine("Decimal Place 1: " + one);
+            Console.WriteLine("Deicmal Place 2: " + two);
             Console.ReadLine();
 
         
